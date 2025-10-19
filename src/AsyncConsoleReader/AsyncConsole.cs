689bfb70bb213454020d62d672b8e9d48cf90179
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2025-10-20 00:27:32 +0900

    change: replaced Read/ReadLine implementation with managed code

--- src/AsyncConsoleReader/AsyncConsole.cs
+++ src/AsyncConsoleReader/AsyncConsole.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using static AsyncConsoleReader.Interop;
+﻿using System.Buffers;
 
 namespace AsyncConsoleReader;
 
@@ -72,205 +70,76 @@ public static class AsyncConsole
 
     public static string? ReadLine(CancellationToken cancellationToken = default)
     {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            CancellationTokenRegistration registration = default;
-            if (cancellationToken.CanBeCanceled)
-            {
-                registration = cancellationToken.UnsafeRegister(_ =>
-                {
-                    var handle = GetStdHandle(STD_INPUT_HANDLE);
-                    CancelIoEx(handle, IntPtr.Zero);
-                }, null);
-            }
-
-            try
-            {
-                return Console.ReadLine();
-            }
-            finally
-            {
-                registration.Dispose();
-            }
-        }
-        else
-        {
-            return ReadLineUnix(cancellationToken);
-        }
-    }
-
-    unsafe static string? ReadLineUnix(CancellationToken cancellationToken)
-    {
-        int* pipefds = stackalloc int[2];
-        pipe(pipefds);
-        int readPipe = pipefds[0];
-        int writePipe = pipefds[1];
-
-        CancellationTokenRegistration registration = default;
-        if (cancellationToken.CanBeCanceled)
-        {
-            registration = cancellationToken.UnsafeRegister(_ =>
-            {
-                unsafe
-                {
-                    byte* buf = stackalloc byte[1];
-                    write(pipefds[1], buf, 1);
-                }
-            }, null);
-        }
+        var buffer = ArrayPool<char>.Shared.Rent(1024);
+        var tail = 0;
+        var pos = 0;
 
         try
         {
-            // Set stdin to non-blocking
-            int flags = fcntl(0, F_GETFL, 0);
-            fcntl(0, F_SETFL, flags | O_NONBLOCK);
-
-            var builder = new DefaultInterpolatedStringHandler();
-            byte* buf = stackalloc byte[1];
-
-            Span<uint> set = stackalloc uint[32];
-
             while (true)
             {
-                FD_ZERO(set);
-                FD_SET(0, set);
-                FD_SET(readPipe, set);
-
-                var nfds = Math.Max(0, readPipe) + 1;
-                var setPtr = NativeMemory.Alloc(128);
-                var setPtrSpan = new Span<byte>(setPtr, 128);
-                MemoryMarshal.AsBytes(set).CopyTo(setPtrSpan);
-
-                var result = select(nfds, (nint)setPtr, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
-                setPtrSpan.CopyTo(MemoryMarshal.AsBytes(set));
-                NativeMemory.Free(setPtr);
-
-                if (FD_ISSET(0, set))
+                var key = ReadKey(true, cancellationToken);
+                switch (key.Key)
                 {
-                    int n = read(0, buf, 1);
-                    if (n > 0)
-                    {
-                        var c = (char)buf[0];
-                        if (c == '\n')
+                    case ConsoleKey.Enter:
+                        Console.WriteLine();
+                        return buffer.AsSpan(0, tail).ToString();
+                    case ConsoleKey.Backspace:
+                        if (pos > 0)
                         {
-                            return builder.ToStringAndClear();
+                            pos--;
+                            tail--;
+
+                            buffer.AsSpan(pos + 1, tail - pos).CopyTo(buffer.AsSpan(pos));
+
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                            Console.Write(buffer.AsSpan(pos, tail - pos).ToString() + " ");
+                            Console.SetCursorPosition(Console.CursorLeft - (tail - pos + 1), Console.CursorTop);
                         }
-                        builder.AppendFormatted(c);
-                    }
-                    else if (n == 0)
-                    {
-                        return builder.ToStringAndClear();
-                    }
-                }
-                else if (FD_ISSET(readPipe, set))
-                {
-                    throw new OperationCanceledException(cancellationToken);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (pos > 0)
+                        {
+                            pos--;
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (pos < tail)
+                        {
+                            pos++;
+                            Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                        }
+                        break;
+                    default:
+                        if (!char.IsControl(key.KeyChar))
+                        {
+                            if (tail >= buffer.Length)
+                            {
+                                var newBuffer = ArrayPool<char>.Shared.Rent(buffer.Length * 2);
+                                buffer.AsSpan(0, tail).CopyTo(newBuffer);
+                                ArrayPool<char>.Shared.Return(buffer);
+                                buffer = newBuffer;
+                            }
+
+                            buffer[tail++] = key.KeyChar;
+                            Console.Write(key.KeyChar);
+                            pos++;
+                        }
+                        break;
                 }
             }
         }
         finally
         {
-            // Restore blocking mode
-            var flags = fcntl(0, F_GETFL, 0);
-            fcntl(0, F_SETFL, flags & ~O_NONBLOCK);
+            ArrayPool<char>.Shared.Return(buffer);
         }
     }
 
     public static int Read(CancellationToken cancellationToken = default)
     {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            CancellationTokenRegistration registration = default;
-            if (cancellationToken.CanBeCanceled)
-            {
-                registration = cancellationToken.UnsafeRegister(_ =>
-                {
-                    var handle = GetStdHandle(STD_INPUT_HANDLE);
-                    CancelIoEx(handle, IntPtr.Zero);
-                }, null);
-            }
-
-            try
-            {
-                return Console.Read();
-            }
-            finally
-            {
-                registration.Dispose();
-            }
-        }
-        else
-        {
-            return ReadUnix(cancellationToken);
-        }
-    }
-
-    unsafe static int ReadUnix(CancellationToken cancellationToken)
-    {
-        int* pipefds = stackalloc int[2];
-        pipe(pipefds);
-        int readPipe = pipefds[0];
-        int writePipe = pipefds[1];
-
-        CancellationTokenRegistration registration = default;
-        if (cancellationToken.CanBeCanceled)
-        {
-            registration = cancellationToken.UnsafeRegister(_ =>
-            {
-                unsafe
-                {
-                    byte* buf = stackalloc byte[1];
-                    write(pipefds[1], buf, 1);
-                }
-            }, null);
-        }
-
-        try
-        {
-            // Set stdin to non-blocking
-            int flags = fcntl(0, F_GETFL, 0);
-            fcntl(0, F_SETFL, flags | O_NONBLOCK);
-
-            byte* buf = stackalloc byte[1];
-            Span<uint> set = stackalloc uint[32];
-
-            FD_ZERO(set);
-            FD_SET(0, set);
-            FD_SET(readPipe, set);
-
-            var nfds = Math.Max(0, readPipe) + 1;
-            var setPtr = NativeMemory.Alloc(128);
-            var setPtrSpan = new Span<byte>(setPtr, 128);
-            MemoryMarshal.AsBytes(set).CopyTo(setPtrSpan);
-
-            var result = select(nfds, (nint)setPtr, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
-            setPtrSpan.CopyTo(MemoryMarshal.AsBytes(set));
-            NativeMemory.Free(setPtr);
-
-            if (FD_ISSET(0, set))
-            {
-                int n = read(0, buf, 1);
-                if (n > 0)
-                {
-                    return buf[0];
-                }
-                else if (n == 0)
-                {
-                    return -1; // EOF
-                }
-            }
-            else if (FD_ISSET(readPipe, set))
-            {
-                throw new OperationCanceledException(cancellationToken);
-            }
-
-            return -1; // Should not reach here
-        }
-        finally
-        {
-            // Restore blocking mode
-            var flags = fcntl(0, F_GETFL, 0);
-            fcntl(0, F_SETFL, flags & ~O_NONBLOCK);
-        }
+        var key = ReadKey(true, cancellationToken);
+        Console.Write(key.KeyChar);
+        return key.KeyChar;
     }
 }
